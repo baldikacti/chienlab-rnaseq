b059fb19e327ba46e3eeb553a201628607495dcc
baldikacti <aldikactiberent@gmail.com>
2023-09-15 09:42:50 -0400

    Updated main.sh to work with GRL10. Added .gtf and new ccna.gtf files

--- bin/fix_gff.R
+++ bin/fix_gff.R
@@ -1,5 +1,9 @@
-library(microseq)
+library(tidyverse)
 
-ref_annot <- readGFF("references/genomic.gff")
+ref_annot <- microseq::readGFF("references/genomic.gff")
 ref_annot$Seqid <- "gi|221232939|ref|NC_011916.1|"
-writeGFF(ref_annot, "references/ccna.gff")
+microseq::writeGFF(ref_annot, "references/ccna.gff")
+
+ref_annot <- microseq::readGFF("references/genomic.gtf")
+ref_annot$Seqid <- "gi|221232939|ref|NC_011916.1|"
+microseq::writeGFF(ref_annot, "references/ccna.gtf")
--- main.sh
+++ main.sh
@@ -2,4 +2,4 @@
 
 # Run pipeline
 
-nextflow run main.nf --data_dir data/QUO1006522/raw --sample_file data/QUO1006522/reference.tsv --cont_tabl data/QUO1006522/contrast_ref.tsv --ref_genome references/NC_011916.fasta --ref_ann references/ccna.gff -profile docker --outdir results/QUO1006522 -resume
+nextflow run main.nf --data_dir data/GRL10/raw --sample_file data/GRL10/reference.tsv --cont_tabl data/GRL10/contrast_ref.tsv --ref_genome references/NC_011916.fasta --ref_ann references/ccna.gff -profile docker --outdir results/GRL10 -resume
